79462bf824407a813c47caa2f865c7b4a738344d
shevapato2008 <shevapato2008@gmail.com>
2017-02-15 03:27:19 -0500

    Initial commit.

---
+++ Prerequisite_OOP2/src/Chicken.java
@@ -0,0 +1,8 @@
+
+public class Chicken extends Bird {
+
+	public Chicken(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+	}
+
+}
---
+++ Prerequisite_OOP2/src/Fish.java
@@ -0,0 +1,17 @@
+
+public class Fish extends Animal {
+	
+	public Fish(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+	}
+
+	public void swim() {
+		System.out.println("Swimming...");
+	}
+
+	@Override
+	public void move() {
+		System.out.println("fish is swimming...");
+	}
+	
+}
