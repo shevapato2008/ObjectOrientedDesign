29ab6523a780d899e0bc9dd806e1bb3b4ccbd548
shevapato2008 <shevapato2008@gmail.com>
2017-02-20 11:50:04 -0500

    Initial Commit.

---
+++ Principle5_DependencyInversion/src/processes/SmartPhoneManufacturingProcess.java
@@ -0,0 +1,29 @@
+package processes;
+
+public class SmartPhoneManufacturingProcess extends GeneralManufacturingProcess {
+	
+	public SmartPhoneManufacturingProcess(String name) {
+		super(name);
+	}
+
+	@Override
+	protected void assembleDevice() {
+		System.out.println("assembled smartphone...");
+	}
+
+	@Override
+	protected void testDevice() {
+		System.out.println("tested smartphone...");
+	}
+
+	@Override
+	protected void packageDevice() {
+		System.out.println("packaged smartphone...");
+	}
+
+	@Override
+	protected void storeDevice() {
+		System.out.println("stored smartphone...");
+	}
+	
+}
