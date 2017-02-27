0f6be8cabb3635ea0cd3e6b69329a8d06659c34f
shevapato2008 <shevapato2008@gmail.com>
2017-02-27 12:51:57 -0500

    Initial Commit.

---
+++ 1.2_Prerequisite_OOP2/src/Sparrow.java
@@ -0,0 +1,12 @@
+public class Sparrow extends Bird implements Flyable {
+
+	public Sparrow(int age, String gender, int weightInLbs) {
+		super(age, gender, weightInLbs);
+	}
+
+	@Override
+	public void fly() {
+		System.out.println("Sparrow flying high...");
+	}
+
+}
---
+++ 1.2_Prerequisite_OOP2/src/Zoo.java
@@ -0,0 +1,23 @@
+public class Zoo {
+
+	public static void main(String[] args) {
+		
+		Animal sparrow1 = new Sparrow(3, "F", 10);
+		sparrow1.eat();
+		sparrow1.sleep();
+		//sparrow1.move();
+		
+		Animal fish1 = new Fish(1, "M", 2);
+		//fish1.move();
+		
+		moveAnimals(fish1);
+		moveAnimals(sparrow1);
+		
+		Flyable flyingBird1 = new Sparrow(1, "M", 4);
+	}
+	
+	public static void moveAnimals(Animal animal) {
+		animal.move();
+	}
+
+}
