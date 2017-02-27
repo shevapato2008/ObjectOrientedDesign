946c8e3a6732020fb9d32633b618aa524c4b32f6
shevapato2008 <shevapato2008@gmail.com>
2017-02-27 13:18:53 -0500

    Initial Commit.

---
+++ 2.5_Principle5_DependencyInversion/src/clients/DeviceFactory.java
@@ -0,0 +1,12 @@
+package clients;
+import processes.GeneralManufacturingProcess;
+import processes.SmartPhoneManufacturingProcess;
+
+public class DeviceFactory {
+
+	public static void main(String[] args) {
+		GeneralManufacturingProcess manufacturer = new SmartPhoneManufacturingProcess("Iphone Process");
+		manufacturer.launchProcess();
+	}
+
+}
